bffc24bd45031348bba86208d3328317067d3748
Matheus Couto <matheusccouto@gmail.com>
2020-08-06 23:31:25 -0300

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit
 
-echo “\
-[general]\n\
-email = \”your-email@domain.com\”\n\
-“ > ~/.streamlit/credentials.toml
-
-echo “\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-“ > ~/.streamlit/config.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
