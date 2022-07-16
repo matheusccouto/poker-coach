46258604845e48d4ee1f9cabbfc84f55dfae42c9
Dennis Steele <onlinechessllc@gmail.com>
2022-07-16 03:10:12 -0400

    darkssage

--- poker_coach/handviz.py
+++ poker_coach/handviz.py
@@ -51,7 +51,7 @@ def hand(
 
     # Create table.
     ax.plot(theta_arr, radius_arr, color="gainsboro", linewidth=5)
-    ax.fill_between(theta_arr, radius_arr, color="limegreen")
+    ax.fill_between(theta_arr, radius_arr, color="darksage")
     ax.axis("off")
 
     # Get hero hand percentage.
